31911bd9aad421be4394684eb130b19ad3a99143
“CristianReyes2023” <cristianlrm2019@gmail.com>
2023-10-11 13:16:11 -0500

    first commit

---
+++ Core/Entities/TipoNotificacion.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core.Entities;
+public class TipoNotificacion : BaseEntity
+{
+    [Required]
+    public string NombreTipoNotificacion { get; set; }
+    [Required]
+    public DateOnly FechaCreacion { get; set; }
+    [Required]
+    public DateOnly FechaModifacion { get; set; }
+    public ICollection<ModuloNotificacion> ModuloNotificaciones { get; set; }
+}
