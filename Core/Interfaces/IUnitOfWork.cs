af57bb4c74ead9ec4a8d2fb3cbe200aec68c1bc7
“CristianReyes2023” <cristianlrm2019@gmail.com>
2023-10-11 22:13:09 -0500

    feat: :sparkles: Entities Repositories and UnitOfWork

--- Core/Interfaces/IUnitOfWork.cs
+++ Core/Interfaces/IUnitOfWork.cs
@@ -6,5 +6,21 @@ using System.Threading.Tasks;
 namespace Core.Interfaces;
 public interface IUnitOfWork 
 {
-    
+    IAuditoria Auditorias {get;}
+    IBlockchain Blockchains {get;}
+    IEstadoNotificacion EstadoNotificaciones {get;}
+    IFormato Formatos {get;}
+    IGenericovsSubmodulos GenericovsSubmodulos {get;}
+    IHiloRespuestaNotificacion HiloRespuestaNotificaciones {get;}
+    IMaestrovsSubmodulos MaestrovsSubmodulos {get;}
+    IModuloNotificacion ModuloNotificaciones {get;}
+    IModulosMaestro ModulosMaestros {get;}
+    IPermisoGenericos PermisoGenericos {get;}
+    IRadicados Radicados {get;}
+    IRol Rols {get;}
+    IRolvsMaestro RolvsMaestros {get;}
+    ISubModulos SubModulos {get;}
+    ITipoNotificacion TipoNotificaciones {get;}
+    ITipoRequerimientos TipoRequerimientos {get;}
+    Task<int> SaveAsync();
 }
